750a4a03ea305cf474373af4700e55939cef7a41
peewoong <fridaymorning99@naver.com>
2023-12-30 13:28:35 +0900

    주석 추가

--- C/ch5.c
+++ C/ch5.c
@@ -3,7 +3,7 @@
 int main(void) {
 	char name[256];
 	printf("이름이 뭐예요?\n");
-	scanf_s("%s", name, (unsigned  int)sizeof(name));
+	scanf_s("%s", name, (unsigned  int)sizeof(name)); // 형변환
 	printf("%s\n", name);
 
 	return 0;
